6895636c2f8b4f172fa3b4e17fb4bc3c911cdc4f
Niranjan Nagumalli <niranjannagumalli@gmail.com>
2025-05-11 01:15:56 +0530

    feat: print name from spreadsheet given email id from discord

---
+++ sheet.ts
@@ -0,0 +1,28 @@
+import { GoogleSpreadsheet } from "npm:google-spreadsheet";
+import { JWT } from "npm:google-auth-library";
+
+export async function getAuth(filePath?: string): Promise<JWT> {
+  const jsonText = await Deno.readTextFile(filePath || "./client_secret.json");
+  const clientSecret = JSON.parse(jsonText);
+  return new JWT({
+    email: clientSecret.client_email,
+    key: clientSecret.private_key,
+    scopes: [
+      "https://www.googleapis.com/auth/spreadsheets",
+      "https://www.googleapis.com/auth/drive",
+    ],
+    keyId: clientSecret.private_key_id,
+  });
+}
+
+export async function getSpreadsheet(
+  sheetId?: string,
+): Promise<GoogleSpreadsheet> {
+  const auth = await getAuth();
+
+  return new GoogleSpreadsheet(
+    sheetId || Deno.env.get("SHEET_ID")!,
+    auth,
+  );
+}
+
