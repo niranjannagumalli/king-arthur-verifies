83d5ca05ea8082b1aaa1a5b6c4c147e56c4e4fb6
Niranjan Nagumalli <niranjannagumalli@gmail.com>
2025-05-12 14:05:10 +0530

    some more checking with the integration of discord calls and sheets call

--- main.ts
+++ main.ts
@@ -10,6 +10,7 @@ import {
 // from Discord.
 import nacl from "https://esm.sh/tweetnacl@v1.0.3?dts";
 import { getVerificationStatus } from "./sheet.ts";
+import { stat } from "node:fs";
 
 // For all requests to "/" endpoint, we want to invoke home() handler.
 serve({
@@ -55,13 +56,15 @@ async function home(request: Request) {
   // It implies that a user has issued a command.
   if (type === 2) {
     const { value } = data.options.find((option) => option.name === "name");
-    // const status = await getVerificationStatus(value);
+    const status = await getVerificationStatus(value);
     return json({
       // Type 4 responds with the below message retaining the user's
       // input at the top.
       type: 4,
       data: {
-        content: `Hello, The status of ${value} is `,
+              content: status
+        ? `Hello, the status of ${value} is ${status}`
+        : `Sorry, no verification record found for ${value}`,
       },
     });
 
