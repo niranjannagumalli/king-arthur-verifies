922f3a8e448d1f8d3d6de85f39d6a63e1b57d2ea
niranjannagumalli <niranjannagumalli@gmail.com>
2026-06-11 21:19:06 +0530

    trial to fix the deployment

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { serve } from "https://deno.land/x/sift@0.6.0/mod.ts";
-import { home } from "./handler.ts";
+import { home } from "./handler.ts"; 
 
 // For all requests to "/" endpoint, we want to invoke home() handler.
 serve({
